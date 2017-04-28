e1ddef87f4dc8d8a49d723d96bbae49b94bb166c
Kory <koryskarbek@yahoo.com>
2017-04-28 08:53:47 -0700

    add view models

---
+++ src/SonOfCod/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using SonOfCod.Models;
+using SonOfCod.ViewModels;
+using System.Security.Claims;
+
+namespace SonOfCod.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SonOfACodDbContext _db;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, FlickrCloneDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
