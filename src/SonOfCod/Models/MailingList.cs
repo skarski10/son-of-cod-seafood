bd423dcedee7d3f58849a91edaebfb8c2e8ed57b
Kory <koryskarbek@yahoo.com>
2017-04-28 08:40:51 -0700

    set up models

---
+++ src/SonOfCod/Models/MailingList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SonOfCod.Models
+{
+    [Table("MailingList")]
+    public class MailingList
+    {
+        [Key]
+        public int MailingListId { get; set; }
+        public string email { get; set; }
+
+    }
+}
---
+++ src/SonOfCod/Models/SonOfACodDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SonOfCod.Models
+{
+    public class SonOfACodDbContext : IdentityDbContext<User>
+    {
+        public SonOfACodDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
